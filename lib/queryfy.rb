36f2f65e70823f45815bc252e69c2fcf0b82594a
bonemind <subhime@gmail.com>
2016-03-03 04:07:21 -0600

    Intetionally broken build for jenkins testing

--- lib/queryfy.rb
+++ lib/queryfy.rb
@@ -12,6 +12,7 @@ module Queryfy
 
 	# Actually builds the query
 	def self.build_query(klass, querystring, orderstring, limit = 50, offset = 0)
+		return 27
 		limit = [max_limit, limit.to_i].min
 		offset = offset.to_i
 		# Handle empty and nil queries
